e5e0f63905d959ede507e126e8fe3d29b2bb05b1
tz1006 <a@omg.tf>
2018-12-23 22:11:15 +0800

    Update email-code.py

--- email-code.py
+++ email-code.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*- 
 # filename: email-code.py
-# discription: Email Notification
+# discription: Email Notification Third Part Server
 
-import
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+ 
+# 第三方 SMTP 服务
+mail_host="smtp.omg.com"  #设置服务器
+mail_user="delete@omg.tf"    #用户名
+mail_pass="-"   #口令 
+ 
+ 
+sender = 'delete@omg.tf'
+receivers = ['c@omg.tf']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+ 
+message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
+message['From'] = Header("菜鸟教程", 'utf-8')
+message['To'] =  Header("测试", 'utf-8')
+ 
+subject = 'Subject'
+message['Subject'] = Header(subject, 'utf-8')
+
+
+try:
+    smtpObj = smtplib.SMTP() 
+    smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+    smtpObj.login(mail_user,mail_pass) 
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print "邮件发送成功"
+except smtplib.SMTPException:
+    print "Error: 无法发送邮件"
